682928769781d4c958b7ddb4f30b14ce00a0164a
Jakub Kułak <jakub.kulak@gmail.com>
2019-04-04 19:21:07 +0200

    Make Pug bounce with gravity

--- public/game.js
+++ public/game.js
@@ -3,7 +3,7 @@ let ctx = canvas.getContext("2d");
 
 const GAME_WIDTH = 300;
 const GAME_HEIGHT = 400;
-const GRAVITY = 60;
+const GRAVITY = 1;
 let playing = true;
 
 const inputHandler = new InputHandler();
@@ -15,24 +15,20 @@ function clear(ctx) {
   ctx.clearRect(0, 0, 300, 400);
 }
 
-let lastTime = 0;
-
 function pause() {
   let pauseDiv = document.getElementById("paused");
   pauseDiv.style.display = playing ? "flex" : "none";
   playing = !playing;
 }
 
-function gameLoop(timeStamp) {
-  console.log(playing);
+function gameLoop() {
+  // console.log(playing);
   if (playing) {
-    let dt = timeStamp - lastTime;
-    lastTime = timeStamp;
     clear(ctx);
-    pug.update(dt);
+    pug.update();
     pug.draw(ctx);
   }
   requestAnimationFrame(gameLoop);
 }
 
-gameLoop(0);
+gameLoop();
--- public/pug.js
+++ public/pug.js
@@ -2,9 +2,17 @@ class Pug {
   constructor(gameWidth, gameHeight, gravity) {
     this.width = 30;
     this.height = 30;
+
+    this.gameHeight = gameHeight;
+
     this.gravity = gravity;
 
-    this.jumpHeight = 100;
+    this.dead = false;
+
+    this.velX = 0;
+    this.velY = 0;
+
+    this.jumpHeight = 15;
 
     this.position = {
       x: gameWidth / 10,
@@ -17,17 +25,27 @@ class Pug {
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
-  update(dt) {
-    // 5 pixels every delta time
-    if (!dt) return;
-    this.position.y += this.gravity / dt;
+  constrain(n, low, high) {
+    return Math.max(Math.min(n, high), low);
+  }
 
+  update() {
+    this.velY = this.constrain(this.velY, -25, 1000);
+    this.velY += this.gravity;
+    this.position.y += this.velY;
+
+    // bounce back
+    if (this.position.y > this.gameHeight) {
+      this.velY = -25;
+    }
+
+    // hit ceiling
     if (this.position.y < 0) {
       this.position.y = 0;
     }
   }
 
   jump() {
-    this.position.y -= this.jumpHeight;
+    this.velY = -this.jumpHeight;
   }
 }
